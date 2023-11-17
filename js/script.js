301cce46466b077acb3df824221675c195b73a3f
asafcastigliano <asafcastigliano@gmail.com>
2023-11-17 17:31:02 +0100

    finito esercizio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let nome = prompt("Inserisci un nome:");
+console.log("Nome:", nome);
+
+let cognome = prompt("Inserisci un cognome:");
+console.log("Cognome:", cognome);
+
+let colore = prompt("Inserisci un colore preferito:");
+console.log("Colore:", colore);
+
+let password = nome + cognome + colore + "23";
+console.log("Passowrd:", password)
